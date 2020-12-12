0ee36fe309afe5eb8978f7933819152db77ceae5
KingEast <imkingeast@gmail.com>
2020-12-12 11:05:57 +0800

    Update ip.js

--- JS/ip.js
+++ JS/ip.js
@@ -46,7 +46,7 @@ emoji=City_ValidCheck(obj['province']) == "香港"? "🇭🇰️":emoji
 emoji=City_ValidCheck(obj['province']) == "澳门"? "️🇲🇴️":emoji
 emoji=City_ValidCheck(obj['province']) == "台湾"? "️️🇹🇼":emoji
 var title =flags.get(obj['countryCode']) + ' '+ City_ValidCheck(obj['city']);//+Area_check(obj['country']);
-var subtitle = ISP_ValidCheck(obj['org']);
+var subtitle = ISP_ValidCheck(obj['isp']) + " ➠ "+ obj['country'];
 var ip = obj['query'];
 var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
 $done({title, subtitle, ip, description});
