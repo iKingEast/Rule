f7c5880f8e01ef196c23fad416375bf700e6079d
KingEast <imkingeast@gmail.com>
2020-12-12 10:55:35 +0800

    Update ip.js

--- JS/ip.js
+++ JS/ip.js
@@ -48,5 +48,5 @@ emoji=City_ValidCheck(obj['province']) == "台湾"? "️️🇹🇼":emoji
 var title =flags.get(obj['countryCode']) + ' '+ City_ValidCheck(obj['city']);//+Area_check(obj['country']);
 var subtitle = ISP_ValidCheck(obj['isp']) + " ➠ "+ obj['country'];
 var ip = obj['query'];
-var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
+var description = '服务商:'+obj['isp'] + '\n'+'定位: [' +obj["latitude"]+","+obj["longitude"]+"]"+ '\n' + 'IP:'+ obj['addr'] + '\n' +'时区:'+ obj['timezone'];
 $done({title, subtitle, ip, description});
